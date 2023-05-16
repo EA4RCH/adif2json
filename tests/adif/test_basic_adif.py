aae8e5f9762ecb8f08fd34b83c3d187dff269eac
César Gallego Rodríguez <gallego.cesar@gmail.com>
2023-05-16 09:09:23 +0200

    feat: more headers testing

--- tests/adif/test_basic_adif.py
+++ tests/adif/test_basic_adif.py
@@ -27,3 +27,32 @@ def test_one_header():
     res = to_dict(imput)
 
     assert res == {"headers": [{'label': 'myheader', 'tipe': None, 'value': '1'}]}
+
+
+def test_multiple_headers():
+    imput = '<myheader:1>1\n<myheader2:2>12\n<EOH>\n'
+    res = to_dict(imput)
+
+    assert res == {"headers": [{'label': 'myheader', 'tipe': None, 'value': '1'},
+                               {'label': 'myheader2', 'tipe': None, 'value': '12'}]}
+
+
+def test_first_header_bad_then_right():
+    imput = '<bad:x>1\n<myheader:1>1\n<EOH>\n'
+    res = to_dict(imput)
+
+    assert res == {"headers": [{'label': 'myheader', 'tipe': None, 'value': '1'}]}
+
+
+def test_all_bad_headers():
+    imput = '<bad:x>1\n<bad2:x>12\n<EOH>\n'
+    res = to_dict(imput)
+
+    assert res == {"headers": []}
+
+
+def test_last_bad_header():
+    imput = '<myheader:1>1\n<bad:x>12\n<EOH>\n'
+    res = to_dict(imput)
+
+    assert res == {"headers": [{'label': 'myheader', 'tipe': None, 'value': '1'}]}
