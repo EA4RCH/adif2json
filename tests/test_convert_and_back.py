51ce34d5a70ff93484a67678855add91a5389f0e
César Gallego Rodríguez <gallego.cesar@gmail.com>
2023-05-19 16:00:43 +0200

    fix: string comparation on test

--- tests/test_convert_and_back.py
+++ tests/test_convert_and_back.py
@@ -2,36 +2,42 @@ from adif2json.adif import to_json
 from adif2json.json import to_adif
 
 
+def _string_similar(s1, s2):
+    a = "".join(s1.split())
+    b = "".join(s2.split())
+    return a == b
+
+
 def test_empty():
-    adif = ''
+    adif = ""
     res = to_adif(to_json(adif))
 
     assert res == adif
 
 
 def test_simple_qso():
-    adif = '<call:6>EA4HFF<EOR>'
+    adif = "<call:6>EA4HFF<EOR>"
     res = to_adif(to_json(adif)).strip()
 
     assert res == adif
 
 
 def test_simple_qso_with_header():
-    adif = '<myheader:1>1<EOH><call:6>EA4HFF<EOR>'
+    adif = "<myheader:1>1<EOH><call:6>EA4HFF<EOR>"
     res = to_adif(to_json(adif)).strip()
 
     assert res == adif
 
 
 def test_multiple_header():
-    adif = '<myheader:1>1<myheader2:2>12<EOH><call:6>EA4HFF<EOR>'
+    adif = "<myheader:1>1<myheader2:2>12<EOH><call:6>EA4HFF<EOR>"
     res = to_adif(to_json(adif)).strip()
 
     assert res == adif
 
 
 def test_multiple_qso():
-    adif = '<call:6>EA4HFF<EOR><call:5>EA4AW<EOR>'
+    adif = "<call:6>EA4HFF<EOR><call:5>EA4AW<EOR>"
     res = to_adif(to_json(adif)).strip()
 
     assert res == adif
@@ -39,19 +45,19 @@ def test_multiple_qso():
 
 def test_multiple_qso_with_multiple_header():
     adif = """
-    <myheader:1>1<myheader2:2>12<EOH>
-    <call:6>EA4HFF<EOR><call:5>EA4AW<EOR>
+        <myheader:1>1<myheader2:2>12<EOH>
+        <call:6>EA4HFF<EOR><call:5>EA4AW<EOR>
     """
     res = to_adif(to_json(adif)).strip()
 
-    assert res == adif
+    assert _string_similar(res, adif)
 
 
 def test_multiple_qso_with_multiple_header_and_types():
     adif = """
-    <myheader:1:N>1<myheader2:2>12<EOH>
-    <call:6:S>EA4HFF<EOR><call:5>EA4AW<EOR>
+        <myheader:1:N>1<myheader2:2>12<EOH>
+        <call:6:S>EA4HFF<EOR><call:5>EA4AW<EOR>
     """
-    res = to_adif(to_json(adif)).strip()
+    res = to_adif(to_json(adif))
 
-    assert res == adif
+    assert _string_similar(res, adif)
