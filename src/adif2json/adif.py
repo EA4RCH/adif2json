2ecfab4d790e8ec3c58175138ad2b9537bcb6012
César Gallego Rodríguez <gallego.cesar@gmail.com>
2023-05-30 14:08:57 +0200

    wip: direct parser

--- src/adif2json/adif.py
+++ src/adif2json/adif.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import adif2json.parser as par
 
 
@@ -7,18 +8,7 @@ from typing import Iterator, Optional, Dict, List
 from dataclasses import dataclass, asdict
 
 
-@dataclass
-class Record:
-    fields: Dict[str, str]
-    type: Optional[str] = None
-    types: Optional[Dict[str, str]] = None
-    errors: Optional[List[Dict[str, str]]] = None
-
-
 def to_json_lines(adif: Iterator[str] | str) -> Iterator[str]:
-    """
-    Must return a json lines iterator.
-    """
     if adif == "":
         return ""
     dicts = to_dict(adif)
@@ -49,12 +39,54 @@ def _custom_asdict_factory(data):
     return dict((k, convert_value(v)) for k, v in data)
 
 
+"""
 def to_dict(adif: Iterator[str] | str) -> Iterator[Dict]:
     records = _read_fields(par.stream_character(adif))
 
     for record in records:
         record_dict = asdict(record, dict_factory=_custom_asdict_factory)
         yield record_dict
+"""
+
+
+@dataclass
+class Record:
+    fields: Dict[str, str]
+    type: Optional[str] = None
+    types: Optional[Dict[str, str]] = None
+    errors: Optional[List[Dict[str, str]]] = None
+
+
+def to_dict(adif: str) -> List[Dict]:
+    def _create_records(
+        acc: List[Record], field: par.Field | par.FormatError | par.Eoh | par.Eor
+    ) -> List[Record]:
+        if acc == []:
+            acc = [Record({})]
+        rec = acc[-1]
+        if isinstance(field, par.Field):
+            rec.fields[field.name] = field.value
+            if field.tipe:
+                if not rec.types:
+                    rec.types = {}
+                rec.types[field.name] = field.tipe
+        elif isinstance(field, par.FormatError):
+            if not rec.errors:
+                rec.errors = []
+            rec.errors.append(asdict(field, dict_factory=_custom_asdict_factory))
+        elif isinstance(field, par.Eoh):
+            rec.type = "headers"
+            acc.append(Record({}))
+        elif isinstance(field, par.Eor):
+            rec.type = "qso"
+            acc.append(Record({}))
+
+        return acc
+
+    fields = par.parse_all(adif)
+
+    records = reduce(_create_records, fields, [Record({})])
+    return list(map(asdict, records))
 
 
 def _read_fields(adif: Iterator[par.Character]) -> Iterator[Record]:
--- src/adif2json/parser.py
+++ src/adif2json/parser.py
@@ -1,6 +1,7 @@
-from typing import Iterator, List, Optional, Tuple
+from functools import reduce
+from typing import Callable, Iterator, List, Optional, Tuple, TypeVar
 from dataclasses import dataclass
-from itertools import dropwhile, takewhile
+from itertools import chain, dropwhile, takewhile
 
 
 @dataclass
@@ -135,3 +136,96 @@ def parse_fields(c: Iterator[Character]) -> Iterator[Field | ParseError | Eor | 
             ok = next(dropwhile(lambda x: not _open_tag(x), c), None)
             if ok is None:
                 return
+
+
+def __unpack(fun):
+    def _fun(val):
+        return fun(*val)
+
+    return _fun
+
+
+@dataclass
+class FormatError:
+    msg: str
+    part: str
+
+
+def parse_all(s: str) -> List[Field | FormatError | Eor | Eoh]:
+    def _parse_record(s: str) -> Tuple[str, str]:
+        try:
+            lab, val = s.split(">", 1)
+            return lab, val
+        except ValueError:
+            if len(s) == 0:
+                return s, ""
+        return "", ""
+
+    def _parse_delimiters(s: str, v: str) -> Tuple[str | Eor | Eoh, str]:
+        up = s.upper()
+        if up == "EOH":
+            return Eoh(), v
+        elif up == "EOR":
+            return Eor(), v
+        return s, v
+
+    def _parse_label(
+        s: str | Eor | Eoh, v: str
+    ) -> Tuple[Tuple[str, int, Optional[str]] | Eor | Eoh | FormatError, str]:
+        if not isinstance(s, str):
+            return s, v
+        parts = s.split(":")
+        name = parts[0]
+        if len(name) == 0:
+            return FormatError("Empty label", s), v
+        try:
+            size = int(parts[1])
+        except ValueError:
+            return FormatError("Size must be a non decimal number", s), v
+        except IndexError:
+            return FormatError("Expect size", s), v
+        if len(parts) > 2:
+            tipe = parts[2]
+            if len(tipe) > 1 or not tipe[0].isalpha():
+                return FormatError("Type must be one Character", s), v
+        else:
+            tipe = None
+        return (name, size, tipe), v
+
+    def _read_field(
+        s: Tuple[str, int, Optional[str]] | Eor | Eoh, v: str
+    ) -> List[Field | FormatError | Eor | Eoh]:
+        if isinstance(s, Eor) or isinstance(s, Eoh):
+            return [s]
+        if isinstance(s, FormatError):
+            return [s]
+        out = []
+        name, size, tipe = s
+        valid = True
+
+        if len(v) == 0:
+            valid = False
+            return [FormatError("Empty value", repr(s))]
+        elif len(v) < size:
+            size = len(v)
+            out.append(FormatError("Truncated value", v))
+        elif len(v) > size:
+            remainder = v[size:]
+            if len(remainder.strip()) > 0:
+                out.append(FormatError("Exeedent value", remainder))
+
+        value = v[:size]
+        out.append(Field(name, value, tipe))
+        return out
+
+    def _reduce(acc, x):
+        if isinstance(x, list):
+            return acc + x
+        return acc + [x]
+
+    tags = s.split("<")
+    records = map(_parse_record, tags[1:])
+    delim = map(__unpack(_parse_delimiters), records)
+    labeled = map(__unpack(_parse_label), delim)
+    records = map(__unpack(_read_field), labeled)
+    return list(reduce(_reduce, records, []))
